357ebdb7e7ce667d5f1206638c09c62586983041
Adwitiya Das <Adwitiya@promact.com>
2018-02-09 16:40:16 +0530

    Corrected an error in Vegetable_StringComapare

--- Vegetable_StringCompare/Vegetable_StringCompare/Program.cs
+++ Vegetable_StringCompare/Vegetable_StringCompare/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Vegetable_StringCompare
 {
@@ -9,19 +10,26 @@ namespace Vegetable_StringCompare
         {
             List<string> veg = new List<string>()
             {
-                "Tomato","Potato","ToMato","Potato","Red Tomato","Tomato Catchup", "Grapes","grapes", "potato"
+                "Tomato","Potato","ToMato","Potato",
+                "Red Tomato","Tomato Catchup", "Grapes",
+                "grapes", "potato"
             };
 
             //-----------------------------------Q1a------------------------------------//
-            var tomato_list = veg.FindAll(x => x.ToLower() == "tomato");
-            Console.WriteLine("The number of tomato strings is: " + tomato_list.Count);
+            var tomato_array = veg.FindAll(x => x.ToLower() == "tomato");
+            Console.WriteLine("The number of tomato strings is: " + tomato_array.Count);
             Console.WriteLine("-----------------------------------------------------");
 
             //-----------------------------------Q1b------------------------------------//
             if (veg.Contains("Potato"))
             {
-                Console.WriteLine("Potato present at index : " + veg.IndexOf("Potato"));
+                for (var i = 0; i < veg.Count; i++)
+                {
+                    if (String.Equals(veg.ElementAt(i), "Potato", StringComparison.Ordinal))
+                        Console.WriteLine("Potato present at index : " + i);
+                }
             }
+
             else
             {
                 Console.WriteLine("Potato not present");
