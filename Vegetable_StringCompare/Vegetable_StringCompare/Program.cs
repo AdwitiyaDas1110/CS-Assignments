f85ba2bf5ed6e96d9e1f17d04f3645cef5727084
Adwitiya Das <Adwitiya@promact.com>
2018-02-10 10:14:04 +0530

    Added CSAssignment 2

---
+++ Vegetable_StringCompare/Vegetable_StringCompare/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vegetable_StringCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> veg = new List<string>()
+            {
+                "Tomato","Potato","ToMato","Potato",
+                "Red Tomato","Tomato Catchup", "Grapes",
+                "grapes", "potato"
+            };
+
+            //-----------------------------------Q1a------------------------------------//
+            var tomato_array = veg.FindAll(x => x.ToLower() == "tomato");
+            Console.WriteLine("The number of tomato strings is: " + tomato_array.Count);
+            Console.WriteLine("-----------------------------------------------------");
+
+            //-----------------------------------Q1b------------------------------------//
+            if (veg.Contains("Potato"))
+            {
+                string index = "Potato present at index : ";
+                for (var i = 0; i < veg.Count; i++)
+                {
+                    if (String.Equals(veg.ElementAt(i), "Potato", StringComparison.Ordinal))
+                        index += i.ToString() + ",";  
+                }
+                Console.WriteLine(index.Substring(0,index.Length-1));
+            }
+            else
+            {
+                Console.WriteLine("Potato not present");
+            }
+            Console.WriteLine("-----------------------------------------------------");
+
+        }
+    }
+}
---
+++ XY_Comparison/XY_Comparison/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace XY_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float X = 0.06f, Y = 5.94f;
+            float sum = X + Y;
+            //-------------------------------Q2a--------------------------//
+            Console.WriteLine("X + Y = " + sum);
+            Console.WriteLine("---------------------------------------------");
+
+
+            //-------------------------------Q2a--------------------------//
+            Console.WriteLine("(X+Y) == 6 ? :" + (sum == 6));
+            Console.WriteLine("---------------------------------------------");
+
+
+            //-------------------------------Q2a--------------------------//
+            Console.WriteLine(((X > Y) ? "X" : "Y") + " has greater value");
+            Console.WriteLine("---------------------------------------------");
+
+        }
+    }
+}
